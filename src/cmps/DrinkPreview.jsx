db5114008d8f2053f3e02b6f3cb48c8a91853301
Arik Alexandrov <arikxl@gmail.com>
2021-07-03 18:36:21 +0300

    responsive

--- src/cmps/DrinkPreview.jsx
+++ src/cmps/DrinkPreview.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const DrinkPreview = ({drink}) => {
     return (
         <div className="list-item">
-            <div>{drink.strDrink}</div>
-
+            <p>{drink.strDrink}</p>
             <div>
                 <img style ={{width: '100px', height: '100px'}}
                  src={drink.strDrinkThumb} alt="{drink.strDrink}" />
