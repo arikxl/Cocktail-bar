9325760d9daae347f6abd40e44a2b5d07cbdd921
Arik Alexandrov <arikxl@gmail.com>
2021-07-03 16:40:46 +0300

    header

--- src/cmps/header/Header.js
+++ src/cmps/header/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header id ="head">
             <div className="logo">
-                <span>Cocktail-Bar</span>
+                <span>Cocktail-Bar TEST</span>
             </div>
         </header>
     )
